252ec3a1f91ace5b99d6e4780cf916c0467af285
Sebastian Schmidt <publicarray@users.noreply.github.com>
2020-03-24 19:04:38 +1100

    Use free when availabe and fallback to freecolor & vm_stat

--- functions/free.fish
+++ functions/free.fish
@@ -5,21 +5,25 @@
 
 function free -d "an osx substitution for linux 'free' command"
 
-	if test (uname) = "Darwin"
+	if command -sq free
+		command free $argv
+	else if command -sq freecolor
+		command freecolor $argv
+	else if command -sq vm_stat
 		# Use vm_stat and sysctl to get system info
 		set vmstat (vm_stat | string match -r "[0-9]+")
 
-		# "Pages free:" 
+		# "Pages free:"
 		set pfree $vmstat[2]
 		# "Pages wired down:"
 		set pwired $vmstat[7]
-		# "Pages inactive:" 
+		# "Pages inactive:"
 		set pinact $vmstat[4]
 		# "Anonymous pages:"
 		set panon $vmstat[15]
 		# "Pages occupied by compressor:"
 		set pcomp $vmstat[17]
-		# "Pages purgeable:" 
+		# "Pages purgeable:"
 		set ppurge $vmstat[8]
 		# "File-backed pages:"
 		set pfback $vmstat[14]
@@ -43,13 +47,13 @@ function free -d "an osx substitution for linux 'free' command"
 		set appmem  (math -s2 "$panon - $ppurge")
 		set used (math -s2 "$appmem + $pwired + $pcomp")
 
-		# Display the hud     
+		# Display the hud
 		printf '                 total     used     free   appmem    wired   compressed\n'
 		printf 'Mem:          %6.2fGb %6.2fGb %6.2fGb %6.2fGb %6.2fGb %6.2fGb\n' $total_mem $used $free $panon $pwired $pcomp
 		printf '+/- Cache:             %6.2fGb %6.2fGb\n' $cached $pinact
 		sysctl -n -o vm.swapusage | awk '{   if( $3+0 != 0 )  printf( "Swap(%2.0f%s):    %6.0fMb %6.0fMb %6.0fMb\n", ($6+0)*100/($3+0), "%", ($3+0), ($6+0), $9+0); }'
 		sysctl -n -o vm.loadavg | awk '{printf( "Load Avg:        %3.2f %3.2f %3.2f\n", $2, $3, $4);}'
 	else
-		command free $argv
+		printf "Your platform is not supported"
 	end
 end
